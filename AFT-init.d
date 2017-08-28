f90515117db27820febc5eaa74bebd9a0fba2e62
Gokul Ravindran <neohex262@gmail.com>
2017-08-28 14:06:10 +0530

    Add files via upload

---
+++ AFT-init.d
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          AFT
+# Required-Start:    $syslog
+# Required-Stop:     $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start AFT server daemon at boot time
+# Description:       Enable AFT server daemon to run automatically on system startup.
+### END INIT INFO
+
+DAEMON="/opt/jhbuild/build/evd/sbin/AFT"
+CONFIG_FILE="/opt/jhbuild/build/evd/etc/AFT/AFT-tls-devel.conf"
+PID_FILE="/var/run/AFT.pid"
+
+case "$1" in
+  start)
+    echo "Starting AFT server daemon"
+    $DAEMON -c $CONFIG_FILE -D
+    ;;
+  stop)
+    echo "Stopping AFT server daemon"
+    kill `cat $PID_FILE`
+    ;;
+  *)
+    echo "Usage: /etc/init.d/AFT {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
